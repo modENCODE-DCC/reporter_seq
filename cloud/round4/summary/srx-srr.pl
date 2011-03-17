7debcf928eb182273cc65d87d589ca530205d74c
zhengzha <zhengzha>
2011-03-17 22:48:18 +0000

    srr for dcc id and geo ids

--- cloud/round4/summary/srx-srr.pl
+++ cloud/round4/summary/srx-srr.pl
@@ -2,14 +2,27 @@
 use URI;
 use Net::FTP;
  
-my $url = "ftp://ftp-trace.ncbi.nih.gov/sra/sra-instant/reads/ByExp/sra/SRX/SRX012/SRX012965";
-my $uri = URI->new($url);
-my ($scheme, $host, $path) = ($uri->scheme, $uri->host, $uri->path);
+my $file = $ARGV[0]; #id-geo-sra-map.csv
+my $host = 'ftp-trace.ncbi.nih.gov';
 my $ftp = Net::FTP->new($host) or die "Cannot connect to, $@";
 $ftp->login();
-
-for my $srr (@{$ftp->ls($path)}) {
-    $ftp->cwd($srr);
-    map {print $scheme, "://", $host, $srr, "/", $_, "\n"} @{$ftp->ls()};
+open my $fh, "<", $file || die;
+while(my $line = <$fh>) {
+    chomp $line;
+    my @fld = split /\t/, $line;
+    if (scalar @fld == 3 && $fld[2] !~ /^\s*$/) {
+	my $url = $fld[2];
+	my $uri = URI->new($url);
+	my ($scheme, $newhost, $path) = ($uri->scheme, $uri->host, $uri->path);
+	if ($newhost ne $host) {
+	    $host = $newhost;
+	    $ftp = Net::FTP->new($newhost) or die "Cannot connect to, $@";
+	    $ftp->login();
+	}
+	for my $srr (@{$ftp->ls($path)}) {
+	    $ftp->cwd($srr);
+	    map {print $fld[0], " ", $fld[1], " ", $scheme, "://", $host, $srr, "/", $_, "\n"} @{$ftp->ls()};
+	}
+    }
 }
 
