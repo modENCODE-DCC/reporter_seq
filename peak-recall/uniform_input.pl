060f8bf1071b70b5ec76dc25b599bd4259b91497
zhengzha <zhengzha>
2011-09-27 22:10:17 +0000

    add knuth random select algo, barcode at least 2 bases long

--- peak-recall/uniform_input.pl
+++ peak-recall/uniform_input.pl
@@ -12,8 +12,14 @@ $Archive::Extract::WARN = 0;
 
 my $rm_barcode = 0;
 my $dent = 0;
+#default, fast select a num of short reads in fastq file to determine barcode
+my $sampling = 'fast';
 my $option = GetOptions ("rm_barcode:i" => \$rm_barcode,
+			 "sampling:s" => \$sampling,
 			 "dent:i" => \$dent);
+#otherwise, use Knuth reservoir sampling algo, much much slower
+$sampling = 'random' unless lc($sampling) eq 'fast';
+
 my ($out, @in) = @ARGV;
 my $out_dir = (fileparse($out))[1];
 my $ori_dir = $out_dir; $ori_dir =~ s/uniform/origin/;
@@ -47,24 +53,25 @@ foreach my $inf (@in) {
 	die "can not found just extracted $t\n" unless -e $t;
 	mprint("done. the new unzipped file is $t", $dent);
     }
-    
+
     if ($rm_barcode) {
 	mprint("checking $t for a barcode...", $dent);
-	$t = remove_barcode($t);
+	$t = remove_barcode($t, $sampling);
 	mprint("done", $dent);
     }
 
-    push @want, $t;
+    push @want, $t if defined($t);
 }
 
-#catenate files
+#catenate files and cleanup tmp files  
 if (scalar @want == 1) {
     system("mv $want[0] $out") == 0 || die "cannot move files \n";
+    system(join(" ", ("rm", @tmp)));
 } else {
     system(join(" ", ("cat", @want, "> $out"))) == 0 || die "cannot cat files \n";  
+    system(join(" ", ("rm", (@want, @tmp))));
 }
-#cleanup files
-system(join(" ", ("rm", (@want, @tmp))));
+
 
 sub mprint {
     my ($msg, $dent) = @_;
@@ -74,116 +81,174 @@ sub mprint {
 }
 
 sub remove_barcode {
-    my ($file) = @_;
-    my $barcode = determine_barcode($file);
+    my ($file, $method) = @_;
+    my $barcode = determine_barcode($file, $method);
     if ($barcode eq "INVALID") {
 	mprint("file being ignored...", $dent);
+	return undef;
     }
     if ( length($barcode) == 0 ) {
 	mprint("file does not appear to contain a barcode...", $dent);
 	return $file;
     }
-    else {
-	mprint("The following barcode was found and will be stripped from file: $barcode", $dent);
-	my $outfile = $file . '.barcode-removed';
-	open( my $IN, "<", $file ) || die "cannot open $file for input $!";
-	open( my $OUT, ">", $outfile ) || die "cannot open $outfile for output $!";
-	my $check_next      = 0;
-        my $barcode_count   = 0;
-        my $nobarcode_count = 0;
-        my $barcode_endpos  = length($barcode);
-        my $badquality_barcode =0;
-	my $qual_next = 0;
-	my $barcode_removed = 0;
-	while ( my $line = <$IN> ) {
-	    chomp $line;
-	    if ( $check_next == 1 && $line =~ /^[ACGTN]*$/) {
-		$check_next=0;
-		if ( $line =~ /^$barcode/ ) {
+    if ($method eq 'fast' && length($barcode) <= 2 ) {
+	mprint("WARNING!!! many short reads might start with the same nucleotide(s) $barcode...", $dent);
+	return $file;
+    }
+
+    mprint("The following barcode was found and will be stripped from file: $barcode", $dent);
+    my $outfile = $file . '.barcode-removed';
+    open( my $IN, "<", $file ) || die "cannot open $file for input $!";
+    open( my $OUT, ">", $outfile ) || die "cannot open $outfile for output $!";
+    my $check_next      = 0;
+    my $barcode_count   = 0;
+    my $nobarcode_count = 0;
+    my $barcode_endpos  = length($barcode);
+    my $badquality_barcode =0;
+    my $qual_next = 0;
+    my $barcode_removed = 0;
+    while ( my $line = <$IN> ) {
+	chomp $line;
+	if ( $check_next == 1 && $line =~ /^[ACGTN]*$/) {
+	    $check_next=0;
+	    if ( $line =~ /^$barcode/ ) {
+		$line = substr( $line, $barcode_endpos );
+		$barcode_count++;
+		$barcode_removed = 1;
+	    } 
+	    else {
+		my $non_matches=0;
+		mprint("Barcode $barcode not found in $line", $dent);
+		for (my $i=0; $i<=$barcode_endpos; $i++) {
+		    $non_matches++ if substr( $line, $i, 1 ) ne substr($barcode, $i, 1);
+		}
+		if ($non_matches <=1) {
+		    $badquality_barcode++;
 		    $line = substr( $line, $barcode_endpos );
-		    $barcode_count++;
 		    $barcode_removed = 1;
 		} 
 		else {
-		    my $non_matches=0;
-		    mprint("Barcode $barcode not found in $line", $dent);
-		    for (my $i=0; $i<=$barcode_endpos; $i++) {
-			$non_matches++ if substr( $line, $i, 1 ) ne substr($barcode, $i, 1);
-		    }
-		    if ($non_matches <=1) {
-			$badquality_barcode++;
-			$line = substr( $line, $barcode_endpos );
-			$barcode_removed = 1;
-		    } 
-		    else {
-			$nobarcode_count++;
-		    }
-		} #endo of $line !~ /^$barcode/
-	    } #end of check_next == 1
-	    elsif ($qual_next == 1) {
-		$qual_next = 0;
-		if ($barcode_removed == 1) {
-		    $barcode_removed = 0;
-		    $line = substr($line, $barcode_endpos);
+		    $nobarcode_count++;
 		}
+	    } #endo of $line !~ /^$barcode/
+	} #end of check_next == 1
+	elsif ($qual_next == 1) {
+	    $qual_next = 0;
+	    if ($barcode_removed == 1) {
+		$barcode_removed = 0;
+		$line = substr($line, $barcode_endpos);
 	    }
-	    else {
-		$check_next = ( $line =~ /^@/ ) ? 1 : 0; 
-		$qual_next = ( $line =~ /^\+/ ) ? 1 : 0;
-	    }
-	    print $OUT ($line . "\n");
-        } #end of while
-        close $OUT;
-	close $IN;
-	mprint("Barcode removal complete: #sequence with barcode= $barcode_count, #sequences without= $nobarcode_count, #of sequences with bad quality barcodes= $badquality_barcode...", $dent);
-        if ( $nobarcode_count == 0 && $barcode_count > 0 ) {
-	    push @tmp, $file;
-	    return $outfile;
-        }        
-	elsif ( $nobarcode_count > 0 ) {
-	    if ( ( ( $barcode_count / ($barcode_count +$nobarcode_count) ) < 0.2 ) ) {
-		my $str = "WARNING: something weird has occured here, barcodes were found in  " . ( $barcode_count / ($barcode_count +$nobarcode_count) ). "% of sequences";
-		mprint($str, $dent);
-		return $file;
-	    }
-	}
-    } #end of length($barcode) != 0
-}
-
-sub determine_barcode {
-    my $filename = shift;
-    my @lines;
-    open( my $IN, "<", $filename )
-	|| die "cannot open $filename to check for barcode $!";
-    my $check_next   = 0;
-    my $lineschecked = 0;
-    while ( my $line = <$IN> ) {
-	last if $lineschecked >= 20000;
-	chomp $line;
-	
-	if ( $check_next == 1 && $line =~ /^[ACGTN]*$/) { 
-	    $check_next = 0;
-	    push( @lines, $line );
 	}
 	else {
 	    $check_next = ( $line =~ /^@/ ) ? 1 : 0; 
+	    $qual_next = ( $line =~ /^\+/ ) ? 1 : 0;
 	}
-	$lineschecked++;
-    }
+	print $OUT ($line . "\n");
+    } #end of while
+    close $OUT;
     close $IN;
-    if ( scalar(@lines) < 10 ) {
-	my $str = "file contains " . scalar(@lines) . " sequences. It does not meet minimum size requirement of 10 sequences and will be skipped\n";
+    mprint("Barcode removal complete: #sequence with barcode= $barcode_count, #sequences without= $nobarcode_count, #of sequences with bad quality barcodes= $badquality_barcode...", $dent);
+    if ( $nobarcode_count == 0 && $barcode_count > 0 ) {
+	push @tmp, $file;
+	return $outfile;
+    }        
+    elsif ( $nobarcode_count > 0 ) {
+	if ( ( ( $barcode_count / ($barcode_count +$nobarcode_count) ) < 0.2 ) ) {
+	    my $str = "WARNING: something weird has occured here, barcodes were found in  " . ( $barcode_count / ($barcode_count +$nobarcode_count) ). "% of sequences";
+	    mprint($str, $dent);
+	    return $file;
+	}
+    }
+}
+
+sub fast_select {
+    my $filename = shift;
+    open( my $IN, "<", $filename )
+	|| die "cannot open $filename to check for barcode $!";
+
+    my $check_next   = 0;
+    my $lineschecked = 0;
+    my @lines;
+    while ( my $line = <$IN> ) {
+        last if $lineschecked >= 20000;
+	chomp $line;
+
+        if ( $check_next == 1 && $line =~ /^[ACGTN]*$/) {
+            $check_next = 0;
+            push( @lines, $line );
+        }
+        else {
+            $check_next = ( $line =~ /^@/ ) ? 1 : 0;
+        }
+        $lineschecked++;
+    }
+
+    close $IN;
+
+    return \@lines;
+}
+
+sub random_select {
+    my ($filename, $num) = @_;
+    open( my $IN, "<", $filename )
+        || die "cannot open $filename to check for barcode $!";
+
+    my @selected;
+    my $numselected = 0;
+    my $check_next = 0;
+    while ( my $line = <$IN> ) {
+	chomp $line;
+	
+	if ( $check_next == 1 && $line =~ /^[ACGTN]*$/) {
+            $check_next = 0;
+	    $numselected++;
+	    if ($numselected > $num) {
+		my $r = int(rand($numselected));
+		if ( $r < $numselected ) {
+		    $selected[$r] = $line;
+		}
+	    }
+	    else {
+		push @selected, $line;
+	    }
+	}
+	else {
+	    $check_next = ( $line =~ /^@/ ) ? 1 : 0;
+	}
+    }
+    
+    close $IN;
+
+    return \@selected;
+}
+
+sub determine_barcode {
+    #random selection, Knuth algorithm R3.4.2 implemented in random_select method, much much slower.
+    my ($filename, $method) = @_;
+    $method = (defined($method) && $method ne '' && lc($method) ne 'fast') ? 'random' : 'fast';
+    my $lines = [];
+    open( my $IN, "<", $filename )
+	|| die "cannot open $filename to check for barcode $!";
+    
+    $lines = fast_select($filename);
+
+    if ( scalar(@$lines) < 10 ) {
+	my $str = "file contains " . scalar(@$lines) . " sequences. It does not meet minimum size requirement of 10 sequences and will be skipped\n";
 	mprint($str, $dent);
 	return "INVALID";
     }
 
+    if ($method eq 'random') {
+        $lines = random_select($filename, scalar @$lines);
+    }
+
     #if it exists but we can't find it with three tries then read quality is so bad it probably isn't worth analyzing anyway
-    my $bc = __test_forbarcode( 0, \@lines );
+    my $bc = __test_forbarcode( 0, $lines );
     if ( $bc eq "" ) {
-	$bc = __test_forbarcode( 10, \@lines );
+	$bc = __test_forbarcode( 10, $lines );
     }
     if ( $bc eq "" ) {
-	$bc = __test_forbarcode( 100, \@lines );
+	$bc = __test_forbarcode( 100, $lines );
     }
     
     return $bc;
